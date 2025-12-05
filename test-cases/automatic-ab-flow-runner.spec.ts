46835d25cebc6d6c5af50ec11a76e2b75e28f2f7
boaz-ritense <boaz.kleijne@ritense.com>
2025-12-05 14:19:30 +0100

    Expanded scenarios for regression test

--- test-cases/automatic-ab-flow-runner.spec.ts
+++ test-cases/automatic-ab-flow-runner.spec.ts
@@ -4,8 +4,20 @@ import { runAbFlow, type FlowOptions } from './automatic-ab-flow';
 // Tests runner
 // Fill in INFRA (Environment), Scenario (from test-scenario-picker) and the LastTask if wanted.
 const scenarios: FlowOptions[] = [
-    { INFRA: 'alo-test', Scenario: 'Default' },
-   // { INFRA: 'alo-test', Scenario: 'C', lastTask: 'vaststellen-persoon-aanvrager' },
+        { INFRA: 'alo-test', Scenario: 'Default' },
+        { INFRA: 'alo-test', Scenario: 'A' },
+        { INFRA: 'alo-test', Scenario: 'B' },
+        { INFRA: 'alo-test', Scenario: 'C' },
+        { INFRA: 'alo-test', Scenario: 'D' },
+        { INFRA: 'alo-test', Scenario: 'E' },
+        { INFRA: 'alo-test', Scenario: 'F' },
+        { INFRA: 'alo-test', Scenario: 'G' },
+        { INFRA: 'alo-test', Scenario: 'H' },
+        { INFRA: 'alo-test', Scenario: 'I' },
+        { INFRA: 'alo-test', Scenario: 'J' },
+        { INFRA: 'alo-test', Scenario: 'K' },
+        { INFRA: 'alo-test', Scenario: 'L' },
+     // { INFRA: 'alo-test', Scenario: 'C', lastTask: 'vaststellen-persoon-aanvrager' },
 ];
 
 test.describe.parallel('AB Flow scenarios', () => {
